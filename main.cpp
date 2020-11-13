f35ea3a8d8b5cacd5d8f3d794386c9ac25d52a5e
jason <394313@bsd48.org>
2020-11-13 08:49:02 -0800

    progress with building heap

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <cstring>
 
 using namespace std;
-
-void create(int* heap, int i); 
-
+void create(int* arr); 
+void buildHeap(int* arr, int i); 
+void display(int* arr); 
 int main(){
+  
   char in[100]; 
-  int* heap = new int[100]; 
+  int* arr = new int[100]; 
   int num;
   int count; 
   cout << "Heap. Commands: create, delete, display, quit" << endl; 
@@ -25,7 +26,6 @@ int main(){
 	//input by file
       }
       else if(strcmp(in, "i")==0){
-	//by manual input
 	count = 0; 
 	while(true){
 	  cout << "input number, -1 to stop: " << endl; 
@@ -34,13 +34,11 @@ int main(){
 	    break; 
 	  }
 	  else{
-	    heap[count] = num;
+	    arr[count] = num;
 	    count++;
 	  }
 	}
-	for(int i = count/2 - 1; i >= 0; i++){
-	  create(heap, i); 
-	}
+	create(arr);
       }
     }
     
@@ -48,7 +46,7 @@ int main(){
       //delete 
     }
     else if(strcmp(in, "display") == 0){
-      //display
+      display(arr);
     }
     else if(strcmp(in, "quit")==0){
       return 0; 
@@ -59,9 +57,35 @@ int main(){
   }
 }
 
-void create(int* heap, int i){
-  int size = sizeOf(heap)/sizeOf(heap[0]);
-  int left = (i*2)+1;
-  int right = (i*2)+2;
-  if(left < 
+void create(int* arr){
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int start = (n/2)-1;
+  for(int i = start; i >= 0; i--){
+    buildHeap(arr, i); 
+  }
+}
+void buildHeap(int* arr, int i){ 
+  int size = sizeof(arr)/sizeof(arr[0]);
+  int top = i;
+  int left = 2*i +1;
+  int right = 2*i +2;
+  if((left < size)&&(arr[left] > arr[top])){
+    top = left; 
+  }
+  if((right < size)&&(arr[right] > arr[top])){
+    top = right; 
+  }
+  if(top != i){
+    int placeholder = arr[i];
+    arr[i] = arr[top];
+    arr[top] = placeholder;
+    buildHeap(arr, top); 
+  }
+}
+void display(int* arr){
+  int size = sizeof(arr)/sizeof(arr[0]);
+  for(int i = 0; i < size; i++){
+    cout << arr[i] << " "; 
+  }
+  cout << endl;
 }
